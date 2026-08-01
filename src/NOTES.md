e97f4f3e23ecd808275ad0aad4ef5efb9badd04e
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2026-08-01 12:59:59 +0300

    added collision and finding the direction

---
+++ src/NOTES.md
@@ -0,0 +1,3 @@
+# Development Notes
+
+Do we always need to initialize all the bricks for the current level?
--- src/build.bat
+++ src/build.bat
@@ -2,7 +2,7 @@
 
 mkdir ..\build
 pushd ..\build
-cl -Zi /Wall "..\src\main.c" /I"C:\lib\raylib-6.0_win64_msvc16\include" ^
+cl -Zi "..\src\main.c" /I"C:\lib\raylib-6.0_win64_msvc16\include" ^
 	/Fe"main.exe" ^
 	/link /libpath:"C:\lib\raylib-6.0_win64_msvc16\lib" ^
 	gdi32.lib msvcrt.lib raylib.lib winmm.lib user32.lib shell32.lib /DEBUG ^
