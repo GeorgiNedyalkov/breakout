fc7c33ac1536175f8c68f6c52985bb981a66b9e3
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2026-08-03 11:23:56 +0300

    move libraries to current directory

--- src/build.bat
+++ src/build.bat
@@ -2,9 +2,9 @@
 
 mkdir ..\build
 pushd ..\build
-cl -Zi "..\src\main.c" /I"C:\lib\raylib-6.0_win64_msvc16\include" ^
+cl -Zi "..\src\main.c" /I"..\resources\raylib\include" ^
 	/Fe"main.exe" ^
-	/link /libpath:"C:\lib\raylib-6.0_win64_msvc16\lib" ^
+	/link /libpath:"..\resources\raylib\lib" ^
 	gdi32.lib msvcrt.lib raylib.lib winmm.lib user32.lib shell32.lib /DEBUG ^
 	/NODEFAULTLIB:libcmt /NODEFAULTLIB:msvcrtd
 popd
